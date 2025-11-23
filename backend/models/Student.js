2081ee8cce8d2d444d3e49f084d3ea607ea523ff
Vainawiyasri Sivakumar <vainawiyasrisivakumar.04@gmail.com>
2025-11-23 17:16:51 +0530

    Delete backend/models directory

--- backend/models/Student.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const StudentSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    age: { type: Number, required: true },
-    course: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Student', StudentSchema);
