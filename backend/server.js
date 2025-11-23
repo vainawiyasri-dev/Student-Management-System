7af3a660867198a9afde41e266aed127902a4c8b
Vainawiyasri Sivakumar <vainawiyasrisivakumar.04@gmail.com>
2025-11-23 16:18:01 +0530

    Add CORS options to server configuration

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: "*", // allow all domains
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // MongoDB connection
@@ -112,3 +117,4 @@ app.get('*', (req, res) => {
 
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
