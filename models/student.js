459225e7289290597c164dda33d4ca39290d37e8
Vainawiyasri Sivakumar <vainawiyasrisivakumar.04@gmail.com>
2025-11-23 11:56:49 +0530

    Update student.js

--- models/student.js
+++ models/student.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const studentSchema = new mongoose.Schema({
+const StudentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     age: { type: Number, required: true },
     course: { type: String, required: true }
 }, { timestamps: true });
 
-module.exports = mongoose.model('student', studentSchema);
+module.exports = mongoose.model('Student', StudentSchema);
